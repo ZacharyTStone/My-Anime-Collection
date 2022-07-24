1263b887859ec241afa24333e9d2daecd2895257
Zach Stone <zachinjapan888@gmail.com>
2022-07-24 16:59:07 -0400

    remove comment from server.js

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ const app = express();
 app.use(express.json());
 
 // protections
-// no content-
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.expectCt());
 app.use(helmet.frameguard());
