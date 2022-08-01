f993a6c03817b71cbdb7121b0aeca05f3f7952e1
Zach Stone <zachinjapan888@gmail.com>
2022-08-01 11:08:22 -0400

    code split react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,58 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Register, Landing, Error, ProtectedRoute } from "./pages";
-import AddAnime from "./pages/Dashboard/AddAnime";
-import MyAnimes from "./pages/Dashboard/MyAnimes";
-import Profile from "./pages/Dashboard/Profile";
-import EditPlaylist from "./pages/Dashboard/EditPlaylist";
-import TopAnimes from "./pages/Dashboard/TopAnimes";
-import SharedLayout from "./pages/Dashboard/SharedLayout";
-import RegisterDemo from "./pages/RegisterDemo";
+import { Landing, Error, ProtectedRoute } from "./pages";
+import { Suspense, lazy } from "react";
+import Loading from "./Components/Atoms/Loading";
+
+const SharedLayout = lazy(() => import("./pages/Dashboard/SharedLayout"));
+const AddAnime = lazy(() => import("./pages/Dashboard/AddAnime"));
+const MyAnimes = lazy(() => import("./pages/Dashboard/MyAnimes"));
+const EditPlaylist = lazy(() => import("./pages/Dashboard/EditPlaylist"));
+const Profile = lazy(() => import("./pages/Dashboard/Profile"));
+const TopAnimes = lazy(() => import("./pages/Dashboard/TopAnimes"));
+const RegisterDemo = lazy(() => import("./pages/RegisterDemo"));
+const Register = lazy(() => import("./pages/Register"));
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <SharedLayout />
-            </ProtectedRoute>
-          }
+    <Suspense
+      fallback={
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            width: "100vw",
+            backgroundColor: "black",
+          }}
         >
-          <Route index path="my-animes" element={<MyAnimes />} />
-          <Route index path="edit-playlist" element={<EditPlaylist />} />
-          <Route index path="add-anime" element={<AddAnime />} />
-          <Route index path="profile" element={<Profile />} />
-          <Route index path="top-animes" element={<TopAnimes />} />
-        </Route>
-        <Route index path="/register" element={<Register />} />
-        <Route index path="/register-demo" element={<RegisterDemo />} />
-        <Route index path="/landing" element={<Landing />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
+          <Loading />
+        </div>
+      }
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <SharedLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route index path="my-animes" element={<MyAnimes />} />
+            <Route index path="edit-playlist" element={<EditPlaylist />} />
+            <Route index path="add-anime" element={<AddAnime />} />
+            <Route index path="profile" element={<Profile />} />
+            <Route index path="top-animes" element={<TopAnimes />} />
+          </Route>
+          <Route index path="/register" element={<Register />} />
+          <Route index path="/register-demo" element={<RegisterDemo />} />
+          <Route index path="/landing" element={<Landing />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
