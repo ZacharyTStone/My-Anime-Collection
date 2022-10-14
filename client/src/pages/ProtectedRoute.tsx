81b9d6d78d1d594994760d69477937e070759316
Zach Stone <83141990+ZacharyTStone@users.noreply.github.com>
2022-10-13 21:55:11 -0400

    change to redirect

--- client/src/pages/ProtectedRoute.tsx
+++ client/src/pages/ProtectedRoute.tsx
@@ -1,19 +1,17 @@
 import { useAppContext } from "../context/appContext";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate, redirect  } from "react-router-dom";
 import React, { useEffect } from 'react';
 
 // not sure what the type for any React component is
 const ProtectedRoute = ({ children }: any) => {
 	
-
-	const navigate = useNavigate()
 	const { user } = useAppContext();
 
 
 	 useEffect(() => {
 		if (user &&  window.location.pathname === "/") {
 			console.log("navigating to my animes")
-			navigate("/my-animes")
+			redirect("/my-animes")
 		}
 
 	 }, [user] );
