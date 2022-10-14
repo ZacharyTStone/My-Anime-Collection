b095ccdb6d3e52989e9e1b135c273a01f09902ad
Zach Stone <83141990+ZacharyTStone@users.noreply.github.com>
2022-10-13 21:41:05 -0400

    try usefect with nav

--- client/src/pages/ProtectedRoute.tsx
+++ client/src/pages/ProtectedRoute.tsx
@@ -1,18 +1,24 @@
 import { useAppContext } from "../context/appContext";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate, useNavigate, useEffect } from "react-router-dom";
 
 // not sure what the type for any React component is
 const ProtectedRoute = ({ children }: any) => {
+	
 
 	const navigate = useNavigate()
 	const { user } = useAppContext();
+
+
+	 useEffect(() => {
+		if (user &&  window.location.pathname === "/") {
+			console.log("navigating to my animes")
+			return <Navigate to="/my-animes" />;
+		}
+
+	 }, [user] );
+
 	if (!user) {
 		return <Navigate to="/landing" />;
-	}
-
-	if (user &&  window.location.pathname === "/") {
-		console.log("navigating to my animes")
-		navigate("/my-animes")
 	}
 	return children;
 };
