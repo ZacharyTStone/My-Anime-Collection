161c6a89829353aa1cac330f877afb3d9edcd9ea
Zach Stone <zachinjapan888@gmail.com>
2022-11-19 09:45:37 -0500

    new logic for language

--- client/src/translations/i18n.js
+++ client/src/translations/i18n.js
@@ -11,11 +11,31 @@ const resources = {
   jp,
 };
 
+const browserLanguageSettings =
+  window.navigator.userLanguage || window.navigator.language;
+
+const language = browserLanguageSettings.split(/[-_]/)[0].toLowerCase(); // language without region code
+
+console.log("language:", language);
+
+const checkLanguage = (language) => {
+  switch (language) {
+    case "en":
+      return "en";
+    case "ja":
+      return "jp";
+    default:
+      return "en";
+  }
+};
+
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: checkLanguage(
+      language
+    ), // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
