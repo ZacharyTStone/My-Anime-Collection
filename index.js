c456ad48bb10a4560efc21f92b7b316e16a8e26d
Zachary Stone <zach.stone.developer@gmail.com>
2023-09-21 09:00:53 -0400

    purged cache and updated tsconfig

---
+++ index.js
@@ -0,0 +1,23 @@
+// path: /src/index.js
+
+// this is just placeholder file
+
+// the application uses the "Concurrently" to run both the client and server at the same time on Heroku
+
+// the behavior is stil as if they were separate. you can also run it locally.
+
+// I would not recommend this approach for a production application. It is better to have a separate server and client in production.
+
+// but since this is a portfolio project I will leave it as is.
+
+// パス：index.js
+
+// これは単なるプレースホルダーファイルです
+
+// アプリケーションはHerokuでクライアントとサーバーを同時に実行するために "Concurrently" を使用します
+
+// それらが別々のものであるかのように振る舞います。また、ローカルで実行することもできます。
+
+// 本番用のアプリケーションにはこのアプローチをお勧めしません。本番用には別々のサーバーとクライアントを持つことが良いです。
+
+// しかし、これはポートフォリオプロジェクトなので、そのままにしておきます。
