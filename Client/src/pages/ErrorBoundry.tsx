a551e975eeed34eeb9119482dcc4068370d9093c
Zach Stone <zach.stone.developer@gmail.com>
2023-10-12 09:16:42 -0400

    fix build issue for error boundary

--- Client/src/pages/ErrorBoundry.tsx
+++ Client/src/pages/ErrorBoundry.tsx
@@ -1,23 +1,77 @@
-import React, { Component } from "react";
+import React, { ReactNode, ErrorInfo } from "react";
+import Loading from "../Components/UI/Loading";
 
-class ErrorBoundary extends Component {
-  constructor(props) {
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    // Update state so the next render will show the fallback UI.
+    console.error(error); // Log the error
     return { hasError: true };
   }
 
-  componentDidCatch(error, errorInfo) {
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
     // You can log the error to an error reporting service
     console.error(error, errorInfo);
   }
 
-  render() {
+  render(): ReactNode {
     if (this.state.hasError) {
-      return <div>Something went wrong!</div>;
+      // You can render any custom fallback UI
+      return (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            width: "100vw",
+            backgroundColor: "black",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <h3
+              style={{
+                color: "var(--primary-500)",
+              }}
+            >
+              {" "}
+              Something Went Wrong...Please Try Again Later!{" "}
+            </h3>
+            <p
+              style={{
+                color: "var(--primary-500)",
+              }}
+            >
+              Please send an email to zach.stone.developer@gmail.com with a
+              description of what happened.
+            </p>
+            {/* Assuming Loading component is implemented correctly */}
+            <Loading />
+          </div>
+        </div>
+      );
     }
 
     return this.props.children;
