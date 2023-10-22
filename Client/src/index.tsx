09bf795b50fb60c640f4aed04d348f742e996fd9
Zach Stone <zach.stone.developer@gmail.com>
2023-10-22 14:34:45 -0400

    update to us React 18 and fix csp

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "normalize.css";
 import "./assets/css/index.css";
 import App from "./App";
 import "./translations/i18n.js";
 import { AppProvider } from "./context/appContext";
+import createRoot from "react-dom/client";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <AppProvider>
       <App />
     </AppProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
