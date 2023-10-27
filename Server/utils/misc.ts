b91e80cf812d994fdb1f670e6eee051d8c664ffb
Zach Stone <Zach.Stone.Developer@gmail.com>
2023-10-26 20:34:23 -0400

    update to use constants

--- Server/utils/misc.ts
+++ Server/utils/misc.ts
@@ -1,8 +1,1 @@
 export const generateRandomNumber = () => Math.floor(Math.random() * 100000);
-
-export const DEMO_USER = {
-  name: "Demo",
-  isDemo: true,
-  email: `DemoUser${generateRandomNumber()}${generateRandomNumber()}${generateRandomNumber()}@demo.com`,
-  password: `${generateRandomNumber()}${generateRandomNumber()}${generateRandomNumber()}`,
-};
